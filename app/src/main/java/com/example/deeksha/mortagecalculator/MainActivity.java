fcd949554c625723928d29f106feab396b476d80
Deeksha Sharma <deeksha.sharma25@gmail.com>
2015-01-18 11:56:14 -0800

    calculation is incorrect

--- app/src/main/java/com/example/deeksha/mortagecalculator/MainActivity.java
+++ app/src/main/java/com/example/deeksha/mortagecalculator/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends ActionBarActivity {
     private CheckBox checkYes;
     private Button calculateButton;
     private SeekBar interestSeekBar;
+    private EditText borrowed;
+    private TextView paymentValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +30,13 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
 
+        interestSeekBar = (SeekBar) findViewById(R.id.seekBar);
 
-         interestSeekBar = (SeekBar) findViewById(R.id.seekBar);
-
-        interestSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-        {
+        interestSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChange = progress;
+                Toast.makeText(getApplicationContext(),"Interest is: "+ new Float(progressChange),Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -51,73 +52,70 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    protected void addButtonActionListener()
-    {
+    protected void addButtonActionListener() {
         calculateButton = (Button) findViewById(R.id.calculate_button);
-        final EditText borrowed = (EditText) findViewById(R.id.amtBorrowed);
-        final TextView paymentValue = (TextView) findViewById(R.id.paymentValue);
+        borrowed = (EditText) findViewById(R.id.amtBorrowed);
+        paymentValue = (TextView) findViewById(R.id.paymentValue);
 
-        calculateButton.setOnClickListener( new View.OnClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 // Amt Borrowed
                 String amtBorrowed = borrowed.getText().toString(); // check this
-                Float borrow = Float.parseFloat(amtBorrowed);
+                Float principal = Float.parseFloat(amtBorrowed);
 
 
                 // Interest rate slider
+                float annualInterest = (float) progressChange;
 
-                float interestToDouble = (float)progressChange;
-//                double total = borrow * progressChange;
-//                payment.setText(Double.toString(total));
-
-
-                // Loan Term Radion buttons
+                // Loan Term Radio buttons
                 loanRadioGroup = (RadioGroup) findViewById(R.id.loanRadioGroup);
                 int radioButtonId = loanRadioGroup.getCheckedRadioButtonId();
-                selectedRadioButton = (RadioButton)findViewById(radioButtonId);
+                selectedRadioButton = (RadioButton) findViewById(radioButtonId);
+
+                int numOfYears;
+                if (selectedRadioButton.getText().equals("7")) {
+                    numOfYears = 7;
+                } else if (selectedRadioButton.getText().equals("15")) {
+                    numOfYears = 15;
+                } else {
+                    numOfYears = 30;
+                }
 
                 // Taxes and Insurance
+                boolean taxes;
                 checkYes = (CheckBox) findViewById(R.id.checkYes);
-                if(checkYes.isChecked())
-                {
-//                    paymentValue.setText(checkYes.getText());
-                }
-                else
-                {
-//                    paymentValue.setText(checkNo.getText());
+                if (checkYes.isChecked()) {
+                    taxes = true;
+                } else {
+                    taxes = false;
                 }
 
+                float monthlyPayment = getMonthlyPayment(principal,annualInterest,numOfYears,taxes);
+
                 // Display value of Monthly payment
-//                paymentValue.setText(selectedRadioButton.getText());
+                paymentValue.setText(Float.toString(monthlyPayment));
             }
         });
     }
 
-    protected float getMonthlyPayment(float principal, float interest,int numOfYears,boolean taxes)
-    {
-        float monthlyInterest = interest/1200;
+    protected float getMonthlyPayment(float principal, float interest, int numOfYears, boolean taxes) {
+        float monthlyInterest = interest / 1200;
         int numOfMonths = numOfYears * 12;
         float taxAmount;
         float monthlyPayment = 0;
 
-        if(taxes == true)
-        {
-            taxAmount = (1/1000)* principal;
-        }
-        else
-        {
+        if (taxes == true) {
+            taxAmount = (1 / 1000) * principal;
+        } else {
             taxAmount = 0;
         }
-        if(interest != 0.0)
-        {
-             monthlyPayment = (float) ((principal * (monthlyInterest/1 - (1/Math.pow(1+monthlyInterest,numOfMonths)))) + taxAmount);
+        if (interest != 0.0) {
+            monthlyPayment = (float) ((principal * (monthlyInterest / 1 - (1 / Math.pow(1 + monthlyInterest, numOfMonths)))) + taxAmount);
 
-        }
-        else if(interest == 0.0)
-        {
-            monthlyPayment = (principal/numOfMonths) + taxAmount;
+        } else if (interest == 0.0) {
+            monthlyPayment = (principal / numOfMonths) + taxAmount;
         }
 
         return monthlyPayment;
