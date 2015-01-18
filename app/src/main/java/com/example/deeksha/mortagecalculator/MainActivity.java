1151c9624299a8e49646da930614d6c88fe0fcd8
Deeksha Sharma <deeksha.sharma25@gmail.com>
2015-01-17 22:03:14 -0800

    calculation done

--- app/src/main/java/com/example/deeksha/mortagecalculator/MainActivity.java
+++ app/src/main/java/com/example/deeksha/mortagecalculator/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.deeksha.mortagecalculator;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +15,13 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-    int progressChange = 0;
+    private int progressChange = 0;
+    private RadioGroup loanRadioGroup;
+    private RadioButton selectedRadioButton;
+    private CheckBox checkYes;
+    private CheckBox checkNo;
+    private Button calculateButton;
+    private SeekBar interestSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +30,13 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-        final SeekBar interestRate = (SeekBar) findViewById(R.id.seekBar);
+         interestSeekBar = (SeekBar) findViewById(R.id.seekBar);
 
-        interestRate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        interestSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
         {
-
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChange = progress;
-                Toast.makeText(getApplicationContext(), "interest rate is "+ progressChange,Toast.LENGTH_SHORT).show();
-//                interestRate
             }
 
             @Override
@@ -44,44 +48,80 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        addButtonActionListener();
+    }
 
-        // button Listener
-        Button calculateButton = (Button) findViewById(R.id.calculate_button);
+
+    protected void addButtonActionListener()
+    {
+        calculateButton = (Button) findViewById(R.id.calculate_button);
         final EditText borrowed = (EditText) findViewById(R.id.amtBorrowed);
-        final TextView payment = (TextView) findViewById(R.id.payment);
+        final TextView paymentValue = (TextView) findViewById(R.id.paymentValue);
 
         calculateButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String amtBorrowed = borrowed.getText().toString(); // check this
-                Double borrow = Double.parseDouble(amtBorrowed);
 
-                double total = borrow * progressChange;
-                payment.setText(Double.toString(total));
+                // Amt Borrowed
+                String amtBorrowed = borrowed.getText().toString(); // check this
+                Float borrow = Float.parseFloat(amtBorrowed);
+
+
+                // Interest rate slider
+
+                float interestToDouble = (float)progressChange;
+//                double total = borrow * progressChange;
+//                payment.setText(Double.toString(total));
+
+
+                // Loan Term Radion buttons
+                loanRadioGroup = (RadioGroup) findViewById(R.id.loanRadioGroup);
+                int radioButtonId = loanRadioGroup.getCheckedRadioButtonId();
+                selectedRadioButton = (RadioButton)findViewById(radioButtonId);
+
+                // Taxes and Insurance
+                checkYes = (CheckBox) findViewById(R.id.checkYes);
+                checkNo = (CheckBox) findViewById(R.id.checkNo);
+                if(checkYes.isChecked() && (!checkNo.isChecked()))
+                {
+//                    paymentValue.setText(checkYes.getText());
+                }
+                else
+                {
+//                    paymentValue.setText(checkNo.getText());
+                }
+
+                // Display value of Monthly payment
+//                paymentValue.setText(selectedRadioButton.getText());
             }
         });
     }
 
+    protected float getMonthlyPayment(float principal, float interest,int numOfYears,boolean taxes)
+    {
+        float monthlyInterest = interest/1200;
+        int numOfMonths = numOfYears * 12;
+        float taxAmount;
+        float monthlyPayment = 0;
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+        if(taxes == true)
+        {
+            taxAmount = (1/1000)* principal;
+        }
+        else
+        {
+            taxAmount = 0;
+        }
+        if(interest != 0.0)
+        {
+             monthlyPayment = (float) ((principal * (monthlyInterest/1 - (1/Math.pow(1+monthlyInterest,numOfMonths)))) + taxAmount);
+
+        }
+        else if(interest == 0.0)
+        {
+            monthlyPayment = (principal/numOfMonths) + taxAmount;
+        }
+
+        return monthlyPayment;
+    }
 }
